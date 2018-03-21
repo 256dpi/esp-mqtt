c3c4694356b61244eef0609d3338a41b0ab7f550
Joël Gähwiler <joel.gaehwiler@gmail.com>
2018-03-21 22:42:14 +0100

    crash on error

--- test/main/main.c
+++ test/main/main.c
@@ -17,7 +17,10 @@ static TaskHandle_t task = NULL;
 
 static void process(void *p) {
   for (;;) {
-    esp_mqtt_publish("hello", (uint8_t *)"world", 5, 0, false);
+    if(!esp_mqtt_publish("hello", (uint8_t *)"world", 5, 0, false)) {
+      ESP_ERROR_CHECK(ESP_FAIL);
+    }
+
     vTaskDelay(1000 / portTICK_PERIOD_MS);
   }
 }
