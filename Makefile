a67e763e90b102cfc3521ef76eadce9c86c81321
Joël Gähwiler <joel.gaehwiler@gmail.com>
2022-01-26 09:39:02 +0100

    test with esp-idf 4

--- Makefile
+++ Makefile
@@ -1,16 +1,21 @@
+fmt:
+	clang-format -i ./*.c ./*.h -style="{BasedOnStyle: Google, ColumnLimit: 120}"
+	clang-format -i ./test/main/*.c -style="{BasedOnStyle: Google, ColumnLimit: 120}"
+
+monitor: test/xtensa-esp32-elf test/esp-idf
+	@clear
+	pyserial-miniterm /dev/cu.SLAB_USBtoUART 115200 --rts 0 --dtr 0 --raw --exit-char 99
+
+# IDFv3
+
 UNAME := $(shell uname)
 
 XTENSA_TOOLCHAIN := "xtensa-esp32-elf-linux64-1.22.0-97-gc752ad5-5.2.0.tar.gz"
-
 ifeq ($(UNAME), Darwin)
 XTENSA_TOOLCHAIN := "xtensa-esp32-elf-macos-1.22.0-97-gc752ad5-5.2.0.tar.gz"
 endif
 
 ESP_IDF_VERSION := "v3.3.5"
-
-fmt:
-	clang-format -i ./*.c ./*.h -style="{BasedOnStyle: Google, ColumnLimit: 120}"
-	clang-format -i ./test/main/*.c -style="{BasedOnStyle: Google, ColumnLimit: 120}"
 
 test/xtensa-esp32-elf:
 	wget https://dl.espressif.com/dl/$(XTENSA_TOOLCHAIN)
@@ -44,10 +49,6 @@ build: test/xtensa-esp32-elf test/esp-idf
 flash: test/xtensa-esp32-elf test/esp-idf
 	export PATH=$(shell pwd)/test/xtensa-esp32-elf/bin:$$PATH; cd ./test; make flash
 
-monitor: test/xtensa-esp32-elf test/esp-idf
-	@clear
-	miniterm.py /dev/cu.SLAB_USBtoUART 115200 --rts 0 --dtr 0 --raw --exit-char 99
-
 idf-monitor: test/xtensa-esp32-elf test/esp-idf test/components/esp-mqtt
 	export PATH=$(shell pwd)/test/xtensa-esp32-elf/bin:$$PATH; cd ./test; make monitor
 
@@ -55,3 +56,44 @@ debug:
 	 export PATH=$(shell pwd)/test/xtensa-esp32-elf/bin:$$PATH; export IDF_PATH=$(shell pwd)/test/esp-idf; ./test/esp-idf/components/espcoredump/espcoredump.py info_corefile -t b64 -c ./test/dump.txt ./test/build/esp-mqtt.elf
 
 run: build flash monitor
+
+# IDFv4
+
+# Note: esptool v3.2 may have issues flashing, if so manually downgrade to v3.1
+
+ESP_IDF_VERSION4 := "v4.3.2"
+
+test4/esp-idf:
+	git clone --recursive  https://github.com/espressif/esp-idf.git test4/esp-idf
+	cd test4/esp-idf; git fetch; git checkout $(ESP_IDF_VERSION4)
+	cd test4/esp-idf/; git submodule update --recursive --init
+
+update4:
+	cd test4/esp-idf; git fetch; git checkout $(ESP_IDF_VERSION4)
+	cd test4/esp-idf/; git submodule update --recursive --init
+
+install4:
+	export IDF_TOOLS_PATH=$(shell pwd)/test4/tools; cd test4/esp-idf; ./install.sh
+
+menuconfig4: test/esp-idf
+	export IDF_TOOLS_PATH=$(shell pwd)/test4/tools; source ./test4/esp-idf/export.sh; cd ./test4; idf.py menuconfig
+
+erase4: test/esp-idf
+	export IDF_TOOLS_PATH=$(shell pwd)/test4/tools; source ./test4/esp-idf/export.sh; cd ./test4; idf.py erase_flash -p /dev/cu.SLAB_USBtoUART
+
+clean4: test/esp-idf
+	export IDF_TOOLS_PATH=$(shell pwd)/test4/tools; source ./test4/esp-idf/export.sh; cd ./test4; idf.py clean
+
+build4: test/esp-idf
+	export IDF_TOOLS_PATH=$(shell pwd)/test4/tools; source ./test4/esp-idf/export.sh; cd ./test4; idf.py build
+
+flash4: test/esp-idf
+	export IDF_TOOLS_PATH=$(shell pwd)/test4/tools; source ./test4/esp-idf/export.sh; cd ./test4; idf.py flash -p /dev/cu.SLAB_USBtoUART -b 921600
+
+idf-monitor4: test/esp-idf test/components/esp-mqtt
+	export IDF_TOOLS_PATH=$(shell pwd)/test4/tools; source ./test4/esp-idf/export.sh; cd ./test4; idf.py monitor -p /dev/cu.SLAB_USBtoUART
+
+debug4:
+	 export IDF_TOOLS_PATH=$(shell pwd)/test4/tools; export IDF_PATH=$(shell pwd)/test/esp-idf; ./test4/esp-idf/components/espcoredump/espcoredump.py info_corefile -t b64 -c ./test/dump.txt ./test/build/esp-mqtt.elf
+
+run4: build4 flash4 monitor
---
+++ test4/main/main.c
@@ -0,0 +1,135 @@
+#include <stdlib.h>
+
+#include <esp_event_loop.h>
+#include <esp_log.h>
+#include <esp_mqtt.h>
+#include <esp_wifi.h>
+#include <nvs_flash.h>
+
+#define WIFI_SSID ""
+#define WIFI_PASS ""
+
+#define MQTT_HOST "public.cloud.shiftr.io"
+#define MQTT_USER "public"
+#define MQTT_PASS "public"
+
+#define MQTT_PORT "1883"
+#define MQTTS_PORT "8883"
+
+// openssl s_client -showcerts -connect public.cloud.shiftr.io:8883 -servername public.cloud.shiftr.io
+extern const uint8_t server_root_cert_pem_start[] asm("_binary_server_root_cert_pem_start");
+extern const uint8_t server_root_cert_pem_end[] asm("_binary_server_root_cert_pem_end");
+
+static void connect() {
+  static bool use_tls = false;
+
+  // cycle use tls
+  use_tls = !use_tls;
+
+  // start mqtt
+  ESP_LOGI("test", "starting mqtt with tls=%d", use_tls);
+  esp_mqtt_tls(use_tls, true, server_root_cert_pem_start, server_root_cert_pem_end - server_root_cert_pem_start);
+  esp_mqtt_start(MQTT_HOST, use_tls ? MQTTS_PORT : MQTT_PORT, "esp-mqtt", MQTT_USER, MQTT_PASS);
+}
+
+static void process(void *p) {
+  for (;;) {
+    // publish roughly every second
+    esp_mqtt_publish("/hello", (uint8_t *)"world", 5, 2, false);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+}
+
+static void restart(void *_) {
+  for (;;) {
+    // stop and start mqtt 15s
+    vTaskDelay(15000 / portTICK_PERIOD_MS);
+    esp_mqtt_stop();
+    connect();
+  }
+}
+
+static esp_err_t event_handler(void *ctx, system_event_t *event) {
+  switch (event->event_id) {
+    case SYSTEM_EVENT_STA_START:
+      // connect to ap
+      esp_wifi_connect();
+
+      break;
+
+    case SYSTEM_EVENT_STA_GOT_IP:
+      // start mqtt
+      connect();
+
+      break;
+
+    case SYSTEM_EVENT_STA_DISCONNECTED:
+      // stop mqtt
+      esp_mqtt_stop();
+
+      // reconnect wifi
+      esp_wifi_connect();
+
+      break;
+
+    default:
+      break;
+  }
+
+  return ESP_OK;
+}
+
+static void status_callback(esp_mqtt_status_t status) {
+  switch (status) {
+    case ESP_MQTT_STATUS_CONNECTED:
+      // subscribe
+      esp_mqtt_subscribe("/hello", 2);
+
+      break;
+
+    case ESP_MQTT_STATUS_DISCONNECTED:
+      // reconnect
+      connect();
+
+      break;
+  }
+}
+
+static void message_callback(const char *topic, uint8_t *payload, size_t len) {
+  ESP_LOGI("test", "incoming: %s => %s (%d)", topic, payload, (int)len);
+}
+
+void app_main() {
+  // initialize nvs flash
+  ESP_ERROR_CHECK(nvs_flash_init());
+
+  // initialize tcp/ip adapter
+  tcpip_adapter_init();
+
+  // register event handler
+  ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL));
+
+  // initialize wifi
+  wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+  ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+  // set wifi storage to ram
+  ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+
+  // set wifi mode
+  ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+
+  // prepare wifi config
+  wifi_config_t wifi_config = {.sta = {.ssid = WIFI_SSID, .password = WIFI_PASS}};
+  ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+
+  // start wifi
+  ESP_ERROR_CHECK(esp_wifi_start());
+
+  // initialize mqtt
+  esp_mqtt_init(status_callback, message_callback, 256, 2000);
+
+  // create tasks
+  xTaskCreatePinnedToCore(process, "process", 2048, NULL, 10, NULL, 1);
+  xTaskCreatePinnedToCore(restart, "restart", 2048, NULL, 10, NULL, 1);
+}
