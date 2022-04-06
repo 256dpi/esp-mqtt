2f72ecde629ceaab76f7dccd81c040f6eefcfbea
Joël Gähwiler <joel.gaehwiler@gmail.com>
2022-04-06 15:24:08 +0200

    use new networking stack in test

--- test/main/main.c
+++ test/main/main.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 
-#include <esp_event_loop.h>
+#include <esp_event.h>
 #include <esp_log.h>
 #include <esp_mqtt.h>
 #include <esp_wifi.h>
@@ -55,28 +55,28 @@ static void restart(void *_) {
   }
 }
 
-static esp_err_t event_handler(void *ctx, system_event_t *event) {
-  switch (event->event_id) {
-    case SYSTEM_EVENT_STA_START:
-      // connect to ap
-      ESP_ERROR_CHECK_WITHOUT_ABORT(esp_wifi_connect());
+static void event_handler(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
+  if (event_base == WIFI_EVENT) {
+    switch (event_id) {
+      case SYSTEM_EVENT_STA_START:
+        // connect to ap
+        ESP_ERROR_CHECK_WITHOUT_ABORT(esp_wifi_connect());
 
-      break;
+        break;
 
-    case SYSTEM_EVENT_STA_GOT_IP:
-      break;
+      case SYSTEM_EVENT_STA_GOT_IP:
+        break;
 
-    case SYSTEM_EVENT_STA_DISCONNECTED:
-      // reconnect Wi-Fi
-      ESP_ERROR_CHECK_WITHOUT_ABORT(esp_wifi_connect());
+      case SYSTEM_EVENT_STA_DISCONNECTED:
+        // reconnect Wi-Fi
+        ESP_ERROR_CHECK_WITHOUT_ABORT(esp_wifi_connect());
 
-      break;
+        break;
 
-    default:
-      break;
+      default:
+        break;
+    }
   }
-
-  return ESP_OK;
 }
 
 static void status_callback(esp_mqtt_status_t status) {
@@ -101,11 +101,14 @@ void app_main() {
   // initialize NVS flash
   ESP_ERROR_CHECK(nvs_flash_init());
 
-  // initialize TCP/IP adapter
-  tcpip_adapter_init();
+  // initialize networking
+  ESP_ERROR_CHECK(esp_netif_init());
 
-  // register event handler
-  ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL));
+  // create default event loop
+  ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+  // enable Wi-Fi
+  esp_netif_create_default_wifi_sta();
 
   // initialize Wi-Fi
   wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
@@ -117,9 +120,14 @@ void app_main() {
   // set wifi mode
   ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
 
+  // register event handlers
+  ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL, NULL));
+  ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL, NULL));
+
   // prepare Wi-Fi config
   wifi_config_t wifi_config = {.sta = {.ssid = WIFI_SSID, .password = WIFI_PASS}};
-  ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+  ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+  ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
 
   // start Wi-Fi
   ESP_ERROR_CHECK(esp_wifi_start());
