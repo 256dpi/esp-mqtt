49e5f2ef24cedb2c25c8fd0814328fb46b9356b3
Joël Gähwiler <joel.gaehwiler@gmail.com>
2023-01-11 15:28:18 +0100

    simplified config

--- esp_mqtt.c
+++ esp_mqtt.c
@@ -552,8 +552,8 @@ bool esp_mqtt_start(const char *host, const char *port, const char *client_id, c
 
   // create mqtt thread
   ESP_LOGI(ESP_MQTT_LOG_TAG, "esp_mqtt_start: create task");
-  BaseType_t ret = xTaskCreatePinnedToCore(esp_mqtt_run, "esp_mqtt", CONFIG_ESP_MQTT_TASK_STACK_SIZE, NULL,
-                                           CONFIG_ESP_MQTT_TASK_STACK_PRIORITY, &esp_mqtt_task, CONFIG_ESP_MQTT_TASK_CORE);
+  BaseType_t ret = xTaskCreatePinnedToCore(esp_mqtt_run, "esp_mqtt", CONFIG_ESP_MQTT_TASK_STACK, NULL,
+                                           CONFIG_ESP_MQTT_TASK_PRIORITY, &esp_mqtt_task, CONFIG_ESP_MQTT_TASK_CORE);
   if (ret != pdPASS) {
     ESP_LOGW(ESP_MQTT_LOG_TAG, "esp_mqtt_start: failed to create task");
     ESP_MQTT_UNLOCK_MAIN();
